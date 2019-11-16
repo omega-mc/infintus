a8bc6482eef7ff2ef4cbb266d56c6e11f594fc24
Draylar <samuelhimes@gmail.com>
2019-11-15 19:25:05 -0600

    fix bracket

--- src/main/java/com/github/draylar/modid/ExampleMod.java
+++ src/main/java/com/github/draylar/modid/ExampleMod.java
@@ -11,15 +11,14 @@ import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer
-{
+public class ExampleMod implements ModInitializer {
+
 	public static final String MODID = "modid";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	public static final ModConfig CONFIG = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new).getConfig();
 
 	@Override
-	public void onInitialize()
-	{
+	public void onInitialize() {
 		Blocks.init();
 		Items.init();
 		Entities.init();
